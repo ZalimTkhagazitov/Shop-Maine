88e9a810feedefe170b6d13cf4d583dda4944c86
Zalim Tkhagazitov <bbulletb@gmail.com>
2021-12-27 00:14:42 -0500

    Part 6 - DB

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,54 @@
+import { ThemeProvider } from "@emotion/react";
+import { Container, createTheme, CssBaseline } from "@mui/material";
+import { useState } from "react";
+import { Route, Switch } from "react-router";
+import { ToastContainer } from "react-toastify";
+import AboutPage from "../../features/about/AboutPage";
+import Catalog from "../../features/catalog/catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css';
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+     palette: {
+       mode: paletteType,
+       background: {
+         default: paletteType === 'light' ? '#eaeaea': '#121212'
+       }
+     }
+  })
+  
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
+    <CssBaseline/>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+      <Container>
+      <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route path='/catalog/:id' component={ProductDetails} />
+          <Route path='/about' component={AboutPage} />
+          <Route path='/contact' component={ContactPage} />
+          <Route path='/server-error' component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
